6869966debf0468acb68841af26718d6a1afb774
Kevin Riexinger <info@kevinriexinger.de>
2022-07-21 16:57:02 +0200

    working Downloader

---
+++ main.py
@@ -0,0 +1,20 @@
+from ast import arg
+from doctest import ELLIPSIS_MARKER
+from pytube import YouTube
+from pytube.cli import on_progress
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("You need to give me a link:)")
+    else:
+        link = argv[1]
+        yt = YouTube(link, on_progress_callback=on_progress)
+
+        print("Title: ", yt.title)
+        print("Creator: ", yt.author)
+        print("Views: ", yt.views)
+        print(f"Length: { yt.length } sek.")
+
+        yd = yt.streams.get_highest_resolution()
+        yd.download("D:\Videos\YouTube")
